e6c810b0436de00a396b3f4e557308377ab07d94
nicmoya <nicolasmoya2024@u.northwestern.edu>
2026-07-23 16:14:34 -0400

    Added full workflow draft, R dockerfile, and R util script.

---
+++ main.nf
@@ -0,0 +1,239 @@
+
+nextflow.enable.dsl=2
+
+/*
+    Parameters
+*/
+
+date = new Date().format('yyyyMMdd')
+log.info("Source: ${params.source}.")
+
+if (params.debug) {
+    println """
+    hdr-nf does not have a debug mode yet.....
+    """
+} else {
+    println """
+    Running hdr-nf in standard mode.
+    """
+}
+
+def bam_dir = [
+    c_elegans   : 'some path',
+    c_tropicalis: 'some path',
+    c_briggsae  : 'some path'
+]
+
+def ref_genome = [
+    c_elegans   : 'some path',
+    c_tropicalis: 'some path',
+    c_briggsae  : 'some path'
+]
+
+def ref_vcf = [
+    c_elegans   : 'some path',
+    c_tropicalis: 'some path',
+    c_briggsae  : 'some path'
+]
+
+def invcf = params.vcf ?: ref_vcf[params.species]
+def ingenome = params.reference ?: ref_genome[params.species]
+def inbam = params.bam ?: bam_dir[params.species]
+
+def log_summary() {
+    // Corrected log summary function to print information instead of recursive call
+    log.info("Workflow summary: \n" + 
+             "Debug mode: ${params.debug}\n" + 
+             "Output directory: ${params.output}\n")
+    
+    // Show help if requested
+    if (params.help) {
+        log.info("Help requested, exiting.")
+        exit 1
+    }
+}
+
+
+
+workflow {
+
+    ch_vcf    = channel.fromPath(invcf, checkIfExists: true)
+    ch_genome = channel.fromPath(ingenome, checkIfExists: true)
+    ch_bam_dir = channel.fromPath(inbam, checkIfExists: true)
+
+    GENERATE_SAMPLE_LIST_AND_WINDOWS(ch_vcf, ch_genome)
+
+    ch_samples = GENERATE_SAMPLE_LIST_AND_WINDOWS.out.sample_list
+    ch_windows = GENERATE_SAMPLE_LIST_AND_WINDOWS.out.windows_bed
+
+    // Fan out: one strain per emission, trimming the newline
+    ch_strains = ch_samples
+        .splitText()
+        .map { line -> line.trim() }
+
+    // One task per strain
+    COUNT_VARIANTS_PER_WINDOW(
+        ch_strains,
+        ch_vcf.first(), //first allows for channel re-usage across ch_strain lines
+        ch_windows.first()
+    )
+
+    ch_all_counts = COUNT_VARIANTS_PER_WINDOW.out.variant_counts
+        .map { strain, tsv -> tsv }
+        .collect()
+
+    MERGE_VARIANT_COUNTS(ch_all_counts)
+
+    MOSDEPTH_COVERAGE(
+        ch_strains,
+        ch_bam_dir.first(),
+        ch_windows.first()
+    )
+
+    ch_all_thresholds = MOSDEPTH_COVERAGE.out.thresholds_bed
+        .map { strain, bed -> bed }
+        .collect()
+
+    MERGE_THRESHOLDS(ch_all_thresholds)
+
+    coverage_ch = MERGE_THRESHOLDS.out.merged_thresholds
+    varct_ch    = MERGE_VARIANT_COUNTS.out.merged_counts
+
+    CALL_HDRS(
+        coverage_ch,
+        varct_ch,
+        ch_windows
+    )
+
+}
+
+
+process GENERATE_SAMPLE_LIST_AND_WINDOWS {
+    tag "${invcf.simpleName}"
+    label ''
+    container 'quay.io/biocontainers/mulled-v2-8186960447c5cb2faa697666dc1e6d919ad23f3e:3127e1b5a6d81d97e2b7e4d53b3b6b0fe1e6023e-0'
+
+    input:
+    path invcf
+    path ingenome
+
+    output:
+    path "samples.txt",  emit: sample_list
+    path "windows.bed",  emit: windows_bed
+
+    script:
+    """
+    bcftools query -l ${invcf} > samples.txt
+
+    samtools faidx ${ingenome}
+    cut -f1,2 ${ingenome}.fai > genome.txt
+    bedtools makewindows -g genome.txt -w 1000 > windows.bed
+    """
+}
+
+process COUNT_VARIANTS_PER_WINDOW {
+    tag "${strain}"
+    label ''
+    container 'quay.io/biocontainers/mulled-v2-8186960447c5cb2faa697666dc1e6d919ad23f3e:3127e1b5a6d81d97e2b7e4d53b3b6b0fe1e6023e-0'
+
+    input:
+    val strain
+    path invcf
+    path windows_bed
+
+    output:
+    tuple val(strain), path("${strain}.variant_counts.tsv"), emit: variant_counts
+
+    script:
+    """
+    bcftools view -s ${strain} ${invcf} | \
+        bcftools filter -i 'GT="alt"' -Oz -o ${strain}.vcf.gz
+
+    bedtools coverage -a ${windows_bed} -b ${strain}.vcf.gz -counts > ${strain}.variant_counts.tsv
+    """
+}
+
+process MERGE_VARIANT_COUNTS {
+    tag "sample_merge"
+    label ''
+    container 'quay.io/biocontainers/mulled-v2-8186960447c5cb2faa697666dc1e6d919ad23f3e:3127e1b5a6d81d97e2b7e4d53b3b6b0fe1e6023e-0'
+
+    input:
+    path variant_count_files
+
+    output:
+    path "all_variant_counts.tsv", emit: merged_counts
+
+    script:
+    """
+    for f in ${variant_count_files}; do
+        strain=\$(basename \$f .variant_counts.tsv)
+        awk -v s="\$strain" 'BEGIN{OFS="\\t"} {print s, \$0}' \$f
+    done > all_variant_counts.tsv
+    """
+}
+
+process MOSDEPTH_COVERAGE {
+    tag "${strain}"
+    label ''
+    container 'quay.io/biocontainers/mosdepth:0.3.8--hd299d5a_0'
+
+    input:
+    val strain
+    path bam_dir
+    path windows_bed
+
+    output:
+    tuple val(strain), path("${strain}.thresholds.bed"), emit: thresholds_bed
+
+    script:
+    """
+    mosdepth ${strain} ${bam_dir}/${strain}.bam -b ${windows_bed} -t 4 -T 1,2,5 -n
+    gunzip ${strain}.thresholds.bed.gz
+    """
+}
+
+process MERGE_THRESHOLDS {
+    tag "merge_thresh"
+    label ''
+    container 'quay.io/biocontainers/mulled-v2-8186960447c5cb2faa697666dc1e6d919ad23f3e:3127e1b5a6d81d97e2b7e4d53b3b6b0fe1e6023e-0'
+
+    input:
+    path threshold_files
+
+    output:
+    path "all_thresholds.tsv", emit: merged_thresholds
+
+    script:
+    """
+    for f in ${threshold_files}; do
+        strain=\$(basename \$f .thresholds.bed)
+        awk -v s="\$strain" 'BEGIN{OFS="\\t"} !/^#/ {print s, \$0}' \$f
+    done > all_thresholds.tsv
+    """
+}
+
+process CALL_HDRS {
+    tag "merge_thresh"
+    label ''
+    container ''
+
+    input:
+    path coverage_df
+    path varct_df
+    path bins_1kb_stripped
+
+    output:
+    path "hdrs.tsv"
+
+    script:
+    """
+    Rscript call_hdr.R \
+        ${coverage_df} \
+        ${varct_df} \
+        ${bins_1kb_stripped} \
+        ${params.refstrain} \
+        ${params.covthresh} \
+        ${params.vcthresh}
+    """
+}
---
+++ utils/call_hdr.R
@@ -0,0 +1,308 @@
+library(plyr)
+library(dplyr)
+library(tidyr)
+library(readr)
+library(valr)
+library(stringr)
+library(data.table)
+
+args <- commandArgs(trailingOnly = TRUE)
+
+coverage_file <- args[[1]]
+varct_file    <- args[[2]]
+windows_file  <- args[[3]]
+REF           <- args[[4]]
+COV_thresh    <- as.numeric(args[[5]])
+VC_thresh     <- as.numeric(args[[6]])
+
+# This function intersects g2g long-read alignments with genome bins to extract bin-level coverage and identity 
+# a miriad of bin metrics are calculated (see mutate cluster)
+alignmentPartition <- function(df) {
+  a2a_bed <- df %>% dplyr::select(REF,S1,E1,IDY,STRAIN) %>%
+    dplyr::rename(chrom=REF,start=S1,end=E1,Identity=IDY)
+  
+  df_LRbins <- valr::bed_intersect(bins_1kb_CB_stripped,a2a_bed) %>%
+    dplyr::rename(CHROM=chrom, START_BIN=start.x, END_BIN=end.x, 
+                  start=start.y, stop=end.y, coverage=.overlap, Identity=Identity.y) %>%
+    dplyr::group_by(CHROM,START_BIN,END_BIN)  %>% 
+    dplyr::filter(coverage==max(coverage)) %>% 
+    dplyr::filter(Identity==max(Identity)) %>% 
+    dplyr::mutate(group_id=cur_group_id()) %>%
+    dplyr::distinct(group_id,.keep_all = T) %>%
+    dplyr::ungroup()
+  
+  df_LRbins_unclass <- bins_1kb_CB_stripped %>% 
+    dplyr::rename(CHROM=chrom, START_BIN=start, END_BIN=end) %>%
+    dplyr::left_join(df_LRbins,by=c("CHROM"="CHROM","START_BIN"="START_BIN","END_BIN"="END_BIN")) %>%
+    dplyr::arrange(CHROM,START_BIN) %>% 
+    dplyr::mutate(coverage=ifelse(is.na(coverage),0,coverage)) %>%
+    dplyr::mutate(STRAIN.y=ifelse(is.na(STRAIN.y),unique(a2a_bed$STRAIN),STRAIN.y)) %>%
+    dplyr::mutate(full_cov = ifelse(coverage == 1e3, T, F)) %>% 
+    dplyr::rename(bin_IDY=Identity,bin_COV=coverage,STRAIN=STRAIN.y) %>%
+    dplyr::select(CHROM,START_BIN,END_BIN,bin_IDY,bin_COV,STRAIN) 
+  
+  return(df_LRbins_unclass)
+}
+
+# given a set of coverage and identity thresholds, this function classifies bins as divergent or non-divergent
+# divergent bins have sub-classifications based on the variant determinants 
+classifyPartition <- function(df,cf,idy) {
+  df_bins_clasif_LR <- df %>%
+    dplyr::mutate(div=ifelse(bin_COV<(cf*10),"C",
+                             ifelse(!(is.na(bin_IDY)) & bin_IDY<=idy,"I","nondiv"))) %>%
+    dplyr::arrange(CHROM,START_BIN) %>%
+    dplyr::group_by(CHROM) %>%
+    dplyr::mutate(div_class = ifelse((lag(div) == "C" | lag(div) == "I") & (lead(div) == "C" | lead(div) == "I") & div=="nondiv","G", div)) %>%
+    dplyr::mutate(div_class = ifelse(div_class=="C" & bin_COV == 0,"Z",div_class)) %>%
+    dplyr::mutate(div_gf=ifelse(div_class=="nondiv","nondiv","div")) %>%
+    dplyr::ungroup()
+  
+  return(df_bins_clasif_LR)
+}
+
+## this function estimates the frequency at which each bin is classified as hyperdivergent in the sampled population
+## low frequency () bins are discarded to avoid super-clustering of divergent regions
+frequencyEstimates <- function(df) {
+  denom<-length(unique(df$STRAIN))
+  df2 <- df %>% 
+    dplyr::group_by(CHROM,START_BIN) %>%
+    dplyr::mutate(binCt=stringr::str_count(div_gf,"nondiv")) %>%
+    dplyr::mutate(binCt=ifelse(is.na(binCt),1,binCt)) %>%
+    dplyr::mutate(binCt=ifelse(binCt==1,0,1)) %>%
+    dplyr::mutate(binCtSum=sum(binCt)) %>%
+    dplyr::mutate(binFreq=(binCtSum/denom)*100)
+  return(df2)
+}
+
+## This function clusters contiguous div bins into div regions, 
+## and assembles a divergent 'footprint' from the string of bin classifications
+clusterBins <- function(df,mode) {
+  
+  if(mode=="SRF") {
+    temp <- df %>% 
+      dplyr::arrange(CHROM,START_BIN) %>%
+      dplyr::mutate(div_gf=ifelse(div_gf =="div" & binFreq < 5,"nondiv",div_gf)) %>%
+      dplyr::select(-binCt,-binCtSum,-binFreq)
+  } else {
+    temp <- df %>% 
+      dplyr::arrange(CHROM,START_BIN) 
+  }
+  
+  temp$enum <- sequence(rle(as.character(temp$div_gf))$lengths)
+  
+  div_bins <- temp %>%
+    dplyr::arrange(CHROM,START_BIN) %>%
+    dplyr::group_by(CHROM,data.table::rleid(div_gf)) %>%
+    dplyr::mutate(gid=cur_group_id()) %>%
+    dplyr::ungroup() %>%
+    dplyr::group_by(CHROM) %>%
+    dplyr::mutate(clusTips=ifelse(div_gf == "div" & lead(enum) == 2 & lead(div_gf)=="div","clust_start", 
+                                  ifelse(div_gf == "div" & lead(enum) > 1 & lead(div_gf)=="div","clust_center",
+                                         ifelse(div_gf == "div" & enum > 1 & lead(div_gf)=="nondiv","clust_end","unclust")))) %>%
+    dplyr::ungroup() %>%
+    dplyr::filter(!(clusTips=="unclust"))
+  
+  if (mode == "LR") {
+    div_regions <- div_bins %>%
+      dplyr::group_by(gid) %>%
+      dplyr::mutate(prop_covz=sum(stringr::str_detect(div_class,"Z"))/n()) %>%
+      dplyr::mutate(prop_lowcov=sum(stringr::str_detect(div_class,"C"))/n()) %>%
+      dplyr::mutate(prop_gf=sum(stringr::str_detect(div_class,"G"))/n()) %>%
+      dplyr::mutate(prop_idy=sum(stringr::str_detect(div_class,"I"))/n()) %>%
+      dplyr::mutate(
+        meanIDY = if (all(is.na(bin_IDY))) NA_real_ else
+          sum(bin_IDY * bin_COV, na.rm = TRUE) / sum(bin_COV[!is.na(bin_IDY)])) %>%
+      dplyr::mutate(meanCov=mean(bin_COV),minStart=min(START_BIN),maxEnd=max(END_BIN),divSize=maxEnd-minStart,group_size=n()) %>%
+      dplyr::mutate(bin_foot=paste0(div_class,collapse = "")) %>%
+      dplyr::distinct(gid,.keep_all = T) %>%
+      dplyr::ungroup() %>%
+      dplyr::select(CHROM,minStart,maxEnd,divSize,meanIDY,meanCov,prop_covz,prop_lowcov,prop_gf,prop_idy,bin_foot,group_size)
+  } else {
+    div_regions <- div_bins %>%
+      dplyr::group_by(gid) %>%
+      dplyr::mutate(prop_covz=sum(stringr::str_detect(div_class,"Z"))/n()) %>%
+      dplyr::mutate(prop_lowcov=sum(stringr::str_detect(div_class,"C"))/n()) %>%
+      dplyr::mutate(prop_gf=sum(stringr::str_detect(div_class,"G"))/n()) %>%
+      dplyr::mutate(prop_idy=sum(stringr::str_detect(div_class,"I"))/n()) %>%
+      dplyr::mutate(meanVC=mean(COUNT),meanCF=mean(pc1X),minStart=min(START_BIN),maxEnd=max(END_BIN),divSize=maxEnd-minStart,group_size=n()) %>%
+      dplyr::mutate(bin_foot=paste0(div_class,collapse = "")) %>%
+      dplyr::distinct(gid,.keep_all = T) %>%
+      dplyr::ungroup() %>%
+      dplyr::select(CHROM,minStart,maxEnd,divSize,meanVC,meanCF,prop_covz,prop_lowcov,prop_gf,prop_idy,bin_foot,group_size)
+  }
+  
+  out <- list(div_bins,div_regions)
+  return(out)
+}
+
+bins_1kb_CB_stripped <- readr::read_tsv(
+  windows_file,
+  col_names = FALSE
+) %>%
+  dplyr::rename(
+    chrom = X1,
+    start = X2,
+    end = X3
+  )
+
+coverage_df <- readr::read_table(
+  coverage_file,
+  col_names = c(
+    "CHROM", "START_BIN", "END_BIN", "NAME",
+    "c1X", "c2X", "c5X", "STRAIN"
+  )
+)
+
+varct_df <- readr::read_table(
+  varct_file,
+  col_names = c(
+    "CHROM", "START_BIN", "END_BIN", "COUNT", "STRAIN"
+  )
+)
+
+#merge bin-based variant counts and coverage data
+SR_stats_WI <- varct_df %>%
+  dplyr::left_join(coverage_df,by=c("STRAIN","CHROM","START_BIN","END_BIN")) %>%
+  dplyr::select(-NAME) %>%
+  dplyr::filter(!(CHROM=="MtDNA")) %>%
+  dplyr::mutate(c1X=ifelse(is.na(c1X),0,c1X)) %>%
+  dplyr::mutate(c2X=ifelse(is.na(c2X),0,c2X)) %>%
+  dplyr::mutate(c5X=ifelse(is.na(c5X),0,c5X)) %>%
+  dplyr::mutate(pc1X=c1X/1e3,pc2X=c2X/1e3,pc5X=c5X/1e3) %>%
+  dplyr::group_by(STRAIN) %>%
+  dplyr::mutate(STRAIN_count=sum(COUNT)) %>% 
+  dplyr::ungroup() %>% 
+  dplyr::mutate(COUNT_ADJ = COUNT / STRAIN_count) 
+
+#classify bins
+all_stats <- SR_stats_WI %>%
+  dplyr::mutate(div_class=ifelse(COUNT >= VC_thresh,"I", #over variant count thresh
+                                 ifelse(pc1X < COV_thresh,"C","R"))) %>%  #under coverage thresh
+  dplyr::mutate(div=ifelse(div_class=="C" | div_class=="I","div","nondiv")) %>%               
+  dplyr::group_by(STRAIN,CHROM) %>%
+  dplyr::mutate(div_gf=ifelse(div=="nondiv" & lead(div)=="div" & lag(div)=="div","div",div)) %>% #fill 1kb gaps
+  dplyr::mutate(div_class=ifelse(div==div_gf,div_class,"G")) %>% #assign a classification to gaps
+  dplyr::ungroup()
+
+  #cluster bins (no frequency filter)
+regList <- list()
+binList <- list()
+for (i in 1:length(strainL)) {
+  #print((i/length(strainL))*100)
+  temp <- all_stats %>% dplyr::filter(STRAIN==strainL[i])
+  div_call <- clusterBins(temp,"SR")
+  #div_call[[1]]$STRAIN <- strainL[[i]]
+  div_call[[2]]$STRAIN <- strainL[i]
+  #binList[[i]] <- div_call[[1]]
+  regList[[i]] <- div_call[[2]]
+}
+
+all_calls_SR <- ldply(regList, data.frame)
+
+#QX calls are spurious (likely repeats) and should be removed from other strain calls
+ref_calls <- all_calls_SR %>% dplyr::filter(STRAIN==REF) %>% dplyr::select(CHROM,minStart,maxEnd)
+
+#find overlaps and remove
+dt_all <- data.table::as.data.table(
+  all_calls_SR[all_calls_SR$STRAIN != REF, ]
+) # all other strain calls
+
+dt_qx <- data.table::as.data.table(ref_calls) # REF calls
+
+data.table::setnames(
+  dt_qx,
+  c("CHROM", "minStart", "maxEnd"),
+  c("CHROM", "start", "end")
+) # rename keys
+
+data.table::setnames(
+  dt_all,
+  c("minStart", "maxEnd"),
+  c("start", "end")
+) # rename keys
+
+data.table::setkey(dt_qx, CHROM, start, end) # set the keys for matching
+data.table::setkey(dt_all, CHROM, start, end) # set the keys for matching
+
+overlaps <- data.table::foverlaps(
+  dt_all,
+  dt_qx,
+  nomatch = 0L,
+  type = "any"
+) # find any overlap
+
+to_drop <- unique(
+  overlaps[, .(CHROM, start = i.start, end = i.end, STRAIN)]
+) # grab all other strain calls that overlap
+
+key_cols <- c("CHROM", "start", "end", "STRAIN")
+
+dt_filtered <- dt_all[
+  !to_drop,
+  on = key_cols
+] # remove them
+
+data.table::setnames(
+  dt_filtered,
+  c("start", "end"),
+  c("minStart", "maxEnd")
+)
+
+all_calls_SR_noREF <- as.data.frame(dt_filtered) # convert back to df
+
+#flag adjacent regions that are 5kb apart
+gap_clust <- all_calls_SR_noREF %>%
+  dplyr::group_by(STRAIN,CHROM) %>%
+  dplyr::mutate(forGapSize=lead(minStart)-maxEnd) %>%
+  dplyr::mutate(flag3g=ifelse(forGapSize<=5000,"clust","noclust")) %>%
+  dplyr::mutate(dec3g=ifelse(flag3g=="clust" ,"join",
+                             ifelse(flag3g=="noclust" & lag(flag3g)=="clust","join","nojoin"))) %>%
+  dplyr::mutate(dec3g=ifelse(is.na(dec3g),"nojoin",dec3g)) %>%
+  dplyr::ungroup()
+
+#get flagged and merged them
+joinClust <- gap_clust %>% 
+  dplyr::filter(dec3g=="join") %>%
+  dplyr::group_by(STRAIN,CHROM) %>%
+  dplyr::mutate(segbreak=ifelse(flag3g=="noclust",paste0(dec3g,row_number()),NA)) %>%
+  tidyr::fill(segbreak,.direction = 'up') %>%
+  dplyr::mutate(gid=data.table::rleid(segbreak)) %>%
+  dplyr::ungroup() %>%
+  dplyr::rowwise() %>%
+  dplyr::mutate(conID=paste0(CHROM,"-",STRAIN,"-",gid)) %>%
+  dplyr::ungroup() %>%
+  dplyr::group_by(conID) %>%
+  dplyr::mutate(newStart=min(minStart),newEnd=max(maxEnd)) %>%
+  dplyr::ungroup() %>%
+  dplyr::rowwise() %>%
+  dplyr::mutate(gapFoot=paste0(rep("R",forGapSize/1e3),collapse = "")) %>%
+  dplyr::mutate(new_foot=ifelse(flag3g=="clust",paste0(bin_foot,gapFoot),bin_foot)) %>%
+  dplyr::ungroup() %>%
+  dplyr::group_by(conID) %>%
+  dplyr::mutate(clust_foot=paste0(new_foot,collapse = "")) %>%
+  dplyr::mutate(newDivSize=newEnd-newStart) %>%
+  dplyr::mutate(newMeanVC=mean(meanVC)) %>%
+  dplyr::mutate(newMeanCF=mean(meanCF)) %>%
+  dplyr::mutate(nclust=n()) %>%
+  dplyr::ungroup() %>%
+  dplyr::distinct(conID,.keep_all = T) %>%
+  dplyr::select(-minStart,-maxEnd,-divSize,-meanVC,-meanCF,-bin_foot) %>%
+  dplyr::rename(minStart=newStart,maxEnd=newEnd,divSize=newDivSize,meanVC=newMeanVC,meanCF=newMeanCF,bin_foot=clust_foot) %>%
+  dplyr::select(CHROM,minStart,maxEnd,divSize,meanVC,meanCF,bin_foot,STRAIN,nclust)
+
+#bind unflagged and merged calls, arrange strains by number of HDRs
+all_calls_SR_clustered <- rbind(joinClust,nojoin) %>%
+  dplyr::group_by(STRAIN) %>%
+  dplyr::mutate(ncalls=n()) %>%
+  dplyr::ungroup() %>%
+  dplyr::arrange(desc(ncalls),STRAIN,CHROM,minStart) %>%
+  dplyr::mutate(sorter=paste0(ncalls,STRAIN)) %>%
+  dplyr::mutate(rleID=data.table::rleid(sorter)) %>%
+  dplyr::group_by(STRAIN) %>%
+  dplyr::mutate(ystrain=cur_group_id()) %>%
+  dplyr::ungroup() 
+
+all_calls_SR_clustered_sfilt <- all_calls_SR_clustered %>%
+  dplyr::filter(divSize >= 5e3)
+
+write.table(all_calls_SR_clustered_sfilt, "hdrs.tsv",row.names = F,quote = F,sep = '\t')
